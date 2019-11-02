ea7c2d89baf35d79786a5461ae81e3b29f94c941
zippermonkey <1960727314@qq.com>
2019-11-02 19:27:18 +0800

    完成基本功能

--- ex2/01.c
+++ ex2/01.c
@@ -1,18 +1,3 @@
-// 初始化表  InitList(L);
-// DestroyList(L)
-//ClearList(L)
-// ListEmpty(L)
-//初始条件是线性表L已存在；操作结果是若L为空表则返回TRUE,否则返回FALSE
-
-// ListLength(L)
-// GetElem(L,i,e) 1<=i<=ListLength(L)
-//LocateElem(L,e)
-//PriorElem(L,cur_e,pre_e)
-//NextElem(L,cur_e,next_e)
-//ListInsert(L,i,e)
-//ListDelete(L,i,e)
-//ListTraverse(L,visit())
-
 /*********************************
  * 
  * 写一个函数来返回某一个元素的前一个元素的地址值会很方便  不存在则返回最后一个节点的位置
@@ -50,20 +35,28 @@ typedef LNode *PtrToLNode; // PtrToLNode 用来(PtrToLNode)malloc(sizeof(LNode));
 typedef PtrToLNode List; //  List用来代表一个表  （带头节点） List L = (List)malloc(sizeof(LNode))  头节点的数据域用来存长度;
 
 /*-------------函数声明-----------------*/
+PtrToLNode GetAdress(List L, int e);             // 返回某一个元素的前一个元素的地址值 不存在则返回最后一个节点的地址
+status isinit(List L);                           // 判断L是否初始化
 status InitList(List *L);                        // 初始化表
 status DestroyList(List *L);                     // 摧毁表
 status ClearList(List L);                        // 清空表
 status ListEmpty(List L);                        // 表是否为空
 status ListLength(List L);                       // 返回线性表长度
 status GetElem(List L, int i, int *e);           // 用e返回第i位置的值
-int LocateElem(List L, int *e);                  // 返回e所在的位序
+int LocateElem(List L, int e);                   // 返回e所在的位序
 status PriorElem(List L, int cur_e, int *pre_e); // 用pre_e 带回 cur_e 的前驱
 status NextElem(List L, int cur_e, int *next_e); // 用next_e带回 cur_e 的后继
+status ListInsert(List L, int i, int e);         // 在i的位置上插入e
+status ListDelete(List L, int i, int *e);        //  删除i的位置的值 用e带出
+status ListTrabverse(List L);                    // 遍历
 
 int main(void)
 {
     List L = NULL;
     int op = 1;
+    // 临时变量
+    int i = 0, e = 0;
+    int cur_e = 0, next_e = 0, pre_e = 0;
     status sta = OK;
     while (op)
     {
@@ -71,99 +64,228 @@ int main(void)
         printf("\n\n");
         printf("      Menu for Linear Table On Sequence Structure \n");
         printf("-------------------------------------------------\n");
-        printf("    	  1. InitList       7. LocateElem\n");
-        printf("    	  2. DestroyList    8. PriorElem\n");
-        printf("    	  3. ClearList       9. NextElem \n");
-        printf("    	  4. ListEmpty     10. ListInsert\n");
-        printf("    	  5. ListLength     11. ListDelete\n");
-        printf("    	  6. GetElem       12. ListTrabverse\n");
-        printf("    	  0. Exit\n");
+        printf("    	  - 1. InitList         - 7. LocateElem\n");
+        printf("    	  - 2. DestroyList      - 8. PriorElem\n");
+        printf("    	  - 3. ClearList        - 9. NextElem \n");
+        printf("    	  - 4. ListEmpty        - 10. ListInsert\n");
+        printf("    	  - 5. ListLength       - 11. ListDelete\n");
+        printf("    	  - 6. GetElem          - 12. ListTrabverse\n");
+        printf("    	  - 0. Exit\n");
         printf("-------------------------------------------------\n");
         printf("    请选择你的操作[0~12]:");
         scanf("%d", &op);
         switch (op)
         {
         case 1:
-            sta = InitList(&L);
-            if (sta == OK)
-                printf("线性表创建成功！\n");
-            else if (sta == OVERFLOW)
-                printf("线性表创建失败！\n");
-            else if (sta == ERROR)
-                printf("线性表已经创建\n");
+            // sta = InitList(&L);
+            // if (sta == OK)
+            //     printf("线性表创建成功！\n");
+            // else if (sta == OVERFLOW)
+            //     printf("线性表创建失败！\n");
+            // else if (sta == ERROR)
+            //     printf("线性表已经创建\n");
+            if (isinit(L))
+                printf("线性表已经创建, 请选择2销毁 或者3清空\n");
+            else
+            {
+                sta = InitList(&L);
+                if (sta == OK)
+                    printf("线性表创建成功！\n");
+                else if (sta == OVERFLOW)
+                    printf("线性表创建失败！\n");
+            }
             getchar();
             getchar();
             break;
         case 2:
-            sta = DestroyList(&L);
-            if (sta == INFEASTABLE)
+            // sta = DestroyList(&L);
+            // if (sta == INFEASTABLE)
+            //     printf("线性表未创建\n");
+            // else if (sta == OK)
+            //     printf("销毁成功\n");
+            if (isinit(L) == FALSE)
                 printf("线性表未创建\n");
-            else if (sta == OK)
+            else
+            {
+                DestroyList(&L);
                 printf("销毁成功\n");
+            }
             getchar();
             getchar();
             break;
         case 3:
-            sta = ClearList(L);
-            if (sta == INFEASTABLE)
+            // sta = ClearList(L);
+            // if (sta == INFEASTABLE)
+            //     printf("线性表未创建\n");
+            // else if (sta == OK)
+            //     printf("线性表清空成功\n");
+            if (isinit(L) == FALSE)
                 printf("线性表未创建\n");
-            else if (sta == OK)
+            else
+            {
+                ClearList(L);
                 printf("线性表清空成功\n");
+            }
             getchar();
             getchar();
             break;
         case 4:
-            sta = ListEmpty(L);
-            if (sta == INFEASTABLE)
+            // sta = ListEmpty(L);
+            // if (sta == INFEASTABLE)
+            //     printf("线性表未创建\n");
+            // else if (sta == TRUE)
+            //     printf("线性表为空\n");
+            // else if (sta == FALSE)
+            //     printf("线性表非空\n");
+            if (isinit(L) == FALSE)
                 printf("线性表未创建\n");
-            else if (sta == TRUE)
-                printf("线性表为空\n");
-            else if (sta == FALSE)
-                printf("线性表非空\n");
+            else
+            {
+                if (ListEmpty(L) == TRUE)
+                    printf("线性表为空\n");
+                else
+                    printf("线性表非空\n");
+            }
             getchar();
             getchar();
             break;
         case 5:
-            sta = ListLength(L);
-            if (sta == ERROR)
-                printf("线性表未创建");
+            // sta = ListLength(L);
+            // if (sta == ERROR)
+            //     printf("线性表未创建\n");
+            // else
+            //     printf("线性表中的元素个数为 %d\n", sta);
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
             else
-                printf("线性表中的元素个数为 %d\n", sta);
+            {
+                printf("线性表中的元素个数为 %d\n", ListLength(L));
+            }
             getchar();
             getchar();
             break;
         case 6:
-            printf("\n----GetElem功能待实现！\n");
+            // printf("请输入要取得元素的位序i: ");
+            // scanf("%d", &i);
+            // sta = GetElem(L, i, &e);
+            // if (sta == ERROR)
+            //     printf("线性表未创建\n");
+            // else if (sta == ERROR)
+            //     printf("i值不合法\n");
+            // else if (sta == OK)
+            //     printf("第%d位元素是%d\n", i, e);
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要取得元素的位序i: ");
+                scanf("%d", &i);
+                sta = GetElem(L, i, &e);
+
+                if (sta == ERROR)
+                    printf("i值不合法\n");
+                else if (sta == OK)
+                    printf("第%d位元素是%d\n", i, e);
+            }
             getchar();
             getchar();
             break;
         case 7:
-            printf("\n----LocateElem功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要查找的元素值：");
+                scanf("%d", &e);
+                sta = LocateElem(L, e);
+                if (sta == NOTFOUND)
+                    printf("线性表中无此元素\n");
+                else
+                    printf("元素%d的位序是%d", e, sta);
+            }
             getchar();
             getchar();
             break;
         case 8:
-            printf("\n----PriorElem功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要查找其前驱的元素值：");
+                scanf("%d", &cur_e);
+                sta = PriorElem(L, cur_e, &pre_e);
+                if (sta == NOTFOUND)
+                    printf("线性表中无此元素\n");
+                else if (sta == ERROR)
+                    printf("%d是第一个元素没有前驱\n", cur_e);
+                else
+                    printf("%d的前驱是%d\n", cur_e, pre_e);
+            }
             getchar();
             getchar();
             break;
         case 9:
-            printf("\n----NextElem功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要查找其后继的元素值：");
+                scanf("%d", &cur_e);
+                sta = NextElem(L, cur_e, &next_e);
+                if (sta == NOTFOUND)
+                    printf("线性表中无此元素\n");
+                else if (sta == ERROR)
+                    printf("%d是最后一个元素没有后继\n", cur_e);
+                else
+                    printf("%d的后继是%d\n", cur_e, next_e);
+            }
             getchar();
             getchar();
             break;
         case 10:
-            printf("\n----ListInsert功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要插入的位置:");
+                scanf("%d", &i);
+                printf("请输入要插入的元素：");
+                scanf("%d", &e);
+                sta = ListInsert(L, i, e);
+                if (sta == ERROR)
+                    printf("插入位置不合法\n");
+                else
+                    printf("插入成功\n");
+            }
             getchar();
             getchar();
             break;
         case 11:
-            printf("\n----ListDelete功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                printf("请输入要删除的位置:");
+                scanf("%d", &i);
+
+                sta = ListDelete(L, i, &e);
+                if (sta == ERROR)
+                    printf("删除位置不合法\n");
+                else
+                    printf("删除%d成功\n", e);
+            }
             getchar();
             getchar();
             break;
         case 12:
-            //printf("\n----ListTrabverse功能待实现！\n");
+            if (isinit(L) == FALSE)
+                printf("线性表未创建\n");
+            else
+            {
+                sta = ListTrabverse(L);
+                if (sta == ERROR)
+                    printf("线性表为空表\n");
+            }
             getchar();
             getchar();
             break;
@@ -174,11 +296,28 @@ int main(void)
     printf("欢迎下次再使用本系统！\n");
 } //end of main()
 
+status isinit(List L)
+{
+    if (L == NULL)
+        return FALSE;
+    else
+        return TRUE;
+}
+
+PtrToLNode GetAdress(List L, int e)
+{
+    // 返回某一个元素的前一个元素的地址值 不存在则返回最后一个节点的地址
+    PtrToLNode p = L;
+    while (p->next && p->next->data != e)
+        p = p->next;
+    return p;
+}
+
 status InitList(List *L)
 {
     // 如果已经存在
-    if (*L)
-        return ERROR;
+    // if (*L)
+    //     return ERROR;
     // 分配空间
     (*L) = (List)malloc(sizeof(LNode)); // 创建一个带头节点的空链表
     if ((*L) == NULL)
@@ -191,9 +330,9 @@ status InitList(List *L)
 
 status DestroyList(List *L)
 {
-    // 未创建
-    if ((*L) == NULL)
-        return INFEASTABLE;
+    // // 未创建
+    // if ((*L) == NULL)
+    //     return INFEASTABLE;
 
     // L是这个表的头节点  最后把free(L)
     // 然后把L为NULL
@@ -214,8 +353,8 @@ status DestroyList(List *L)
 status ListEmpty(List L)
 {
     // 未创建
-    if (L == NULL)
-        return INFEASTABLE;
+    // if (L == NULL)
+    //     return INFEASTABLE;
     if (L->data == 0)
         return TRUE;
     else
@@ -227,35 +366,34 @@ status ClearList(List L)
     // free 其他节点
 
     // 未创建
-    if (L == NULL)
-        return INFEASTABLE;
+    // if (L == NULL)
+    //     return INFEASTABLE;
     PtrToLNode t = L->next;
     if (ListEmpty(L) == TRUE)
         return OK;
     else
         DestroyList(&t);
-
     L->next = NULL;
     L->data = 0;
     return OK;
 }
-status ListLength(List L)
+int ListLength(List L)
 {
     // 返回线性表长度
 
-    if (L == NULL) //未创建
-        return INFEASTABLE;
-    else
-        return L->data;
+    // if (L == NULL) //未创建
+    //     return INFEASTABLE;
+    // else
+    return L->data;
 }
 status GetElem(List L, int i, int *e)
 {
-    if (L == NULL) //未创建
-        return INFEASTABLE;
-    if (ListEmpty(L) == 0)
-        return ERROR;
+    // if (L == NULL) //未创建
+    //     return INFEASTABLE;
+    // if (ListEmpty(L) == 0)
+    //     return ERROR;
 
-    if (i < 0 || i > ListLength(L)) //i 不合法
+    if (i < 1 || i > ListLength(L)) //i 不合法
         return ERROR;
 
     int k = 0;
@@ -267,19 +405,12 @@ status GetElem(List L, int i, int *e)
     *e = p->data;
     return OK;
 }
-PtrToLNode GetAdress(List L, int e)
-{
-    // 返回某一个元素的前一个元素的地址值 不存在则返回最后一个节点的地址
-    PtrToLNode p = L;
-    while (p->next && p->next->data != e)
-        p = p->next;
-    return p;
-}
-int LocateElem(List L, int *e)
+
+int LocateElem(List L, int e)
 {
     // 返回e所在的为序  从1开始
-    if (L == NULL) //未创建
-        return INFEASTABLE;
+    // if (L == NULL) //未创建
+    //     return INFEASTABLE;
 
     int i = 1;
     PtrToLNode p = L->next;
@@ -291,13 +422,13 @@ int LocateElem(List L, int *e)
     if (p) // 如果找到
         return i;
     else //没找到
-        return ERROR;
+        return NOTFOUND;
 }
 
 status PriorElem(List L, int cur_e, int *pre_e)
 {
-    if (L == NULL) //未创建
-        return INFEASTABLE;
+    // if (L == NULL) //未创建
+    //     return INFEASTABLE;
 
     PtrToLNode p = GetAdress(L, cur_e);
     if (p->next == NULL) // 没有这一个元素
@@ -306,7 +437,78 @@ status PriorElem(List L, int cur_e, int *pre_e)
         return ERROR;
     else
     {
-        *pre_e = p->next->data;
+        *pre_e = p->data;
         return OK;
     }
+}
+
+status NextElem(List L, int cur_e, int *next_e)
+{
+    // if (L == NULL) //未创建
+    //     return INFEASTABLE;
+
+    PtrToLNode p = GetAdress(L, cur_e);
+    if (p->next == NULL) // 没有这一个元素
+        return NOTFOUND;
+    else if (p->next->next == NULL) // 最后一个元素  没有后继
+        return ERROR;
+    else
+    {
+        *next_e = p->next->next->data;
+        return OK;
+    }
+}
+
+status ListInsert(List L, int i, int e)
+{
+    //在i的位置上插入e
+    PtrToLNode p = L; // p为要插入的位置的前一个位置
+
+    if (i < 1 || i > ListLength(L) + 1) //i 不合法
+        return ERROR;
+    else
+        while (--i)
+            p = p->next;
+    PtrToLNode q = (PtrToLNode)malloc(sizeof(LNode));
+    q->data = e;
+    q->next = p->next;
+    p->next = q;
+    L->data++;
+    return OK;
+}
+
+status ListDelete(List L, int i, int *e)
+{
+    // 删除i的位置的值 用e带出
+
+    PtrToLNode p = L;               // p为要删除的位置的前一个位置
+    if (i < 1 || i > ListLength(L)) //i 不合法
+        return ERROR;
+    else
+        while (--i)
+            p = p->next;
+    PtrToLNode q = p->next;
+    p->next = q->next;
+    *e = q->data;
+    free(q);
+    L->data--;
+    return OK;
+}
+status ListTrabverse(List L)
+{ // 遍历
+    if (ListEmpty(L) == TRUE)
+        return ERROR;
+    else
+    {
+        PtrToLNode p = L->next;
+        while (p)
+        {
+            printf("%d", p->data);
+            p = p->next;
+            if (p)
+                printf("  -->  ");
+        }
+    }
+    printf("\n");
+    return OK;
 }
